1db65e56817ede42232a0f7e910df8a94ef4ed57
gaurav-210 <karan02555@gmail.com>
2018-06-12 20:39:15 +0530

    add assignment of class 2

---
+++ assignment2.py
@@ -0,0 +1,33 @@
+# Q1. print anything you want on screen
+
+print("hi \n"
+      "I'm  Gaurav chauhan \n"
+      "Currently I'm Persuing Bachelor Of Technology In Electronics And Communication engineering")
+
+# Q2. join  two strings using +
+
+print("Gautrav "  + "Chauhan")
+
+# Q3 . take the input of 3 variable x,y and 2 . priont their value on screen
+
+x=int (input("Enter A Number: "))
+y=int (input("Enter Another Number: "))
+z=(x+y)
+print(z)
+
+# Q4 . print lets's get started on screen
+
+print("Let's Get Started")
+
+# Q5 . print the following values using placeholder s= "acadview" course="python" fees=5000 "
+
+s = "acadview"
+course =" python "
+fees = "5000 "
+print("%s  %s  %s " %(s,course,fees) )
+
+# Q6 . let's do some exercise  name ="Tony stark" salary=1000000 print('%s''%d')%(_____,_____)
+
+name= "Tony stark"
+salary= 1000000
+print("%s  %d"%(name,salary))
