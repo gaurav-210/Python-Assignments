a210db04234a81c1771be604a6bbd58b84502684
gaurav-210 <karan02555@gmail.com>
2018-06-14 11:36:40 +0530

    add some modifications in class 3 assignments

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -89,7 +89,7 @@ print(queue)
 
 
 li = [1,2,3,4,5,6,7,8,9,99,100]
-even =[i for i in li if i%2 !=0]
-odd=[i for i in li if i%2 == 0]
+odd =[i for i in li if i%2 !=0]
+even=[i for i in li if i%2 == 0]
 print(even)
 print(odd)
